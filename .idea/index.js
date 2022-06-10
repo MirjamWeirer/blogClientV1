13c236164c0a7375bea8a510bc6f5f7005370545
MirjamWeirer <mirjam.weirer@gmx.at>
2022-06-10 09:39:33 +0200

    Import RandomBytes um die Eindeutige Id zu erstellen

--- .idea/index.js
+++ .idea/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {randomBytes} = require('crypto')
 
 const app = express();
 
@@ -8,7 +9,9 @@ app.get('/posts', (req, res) => {
     res.send(posts);
 });
 
-app.post('/posts', (req, res) => {});
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+});
 
 app.listen(4000, () => {
     console.log('Listening on 4000');
